29bbb3ae91b938808417734353a7fd6ab6d3670a
jsta <jstachelek@utexas.edu>
2017-07-31 11:28:14 -0400

    strict regex matching

--- R/surfget.R
+++ R/surfget.R
@@ -8,6 +8,7 @@
 #'@param fdir character file path to local data directory
 #'@examples \dontrun{ 
 #'surfs <- surfget(rnge = c(201402, 201410), param = "chlext")
+#'surfs <- surfget(rnge = 201509, param = "sal")
 #'}
 
 surfget <- function(rnge, param, fdir = getOption("fdir")){
@@ -16,10 +17,15 @@ surfget <- function(rnge, param, fdir = getOption("fdir")){
   }
   
   basepath <- file.path(fdir, "DF_Surfaces")
+  
   flist <- list.files(basepath, include.dirs = T, full.names = T)
-  flist <- suppressWarnings(flist[which(as.numeric(substring(basename(flist),1,6)) >= rnge[1])])
-  flist <- suppressWarnings(flist[which(as.numeric(substring(basename(flist),1,6)) <= rnge[2])])
-  flist <- list.files(flist, paste0(param, ".tif$"), full.names = TRUE, include.dirs = TRUE)
+  flist <- suppressWarnings(flist[
+      which(as.numeric(substring(basename(flist), 1, 6)) >= rnge[1])
+    ])
+  flist <- suppressWarnings(flist[
+      which(as.numeric(substring(basename(flist), 1, 6)) <= rnge[2])
+    ])
+  flist <- list.files(flist, paste0("^", param, ".tif$"), full.names = TRUE, include.dirs = TRUE)
   
   suppressWarnings(raster::stack(flist))
 }
