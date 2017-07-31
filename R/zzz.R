5a2427051ddd2e5ceac3d64a1b796efbd6b96b8a
jsta <jstachelek@utexas.edu>
2017-07-31 11:40:57 -0400

    lint packagestartup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,11 @@
 .onLoad <- function(libname = find.package("DataflowR"), pkgname = "DataflowR"){
   
   localpath <- system.file("localpath", package = "DataflowR")
-  fdir <- matrix(utils::read.delim(localpath, header = FALSE, stringsAsFactors = FALSE))[[1]][1]
+  fdir <- matrix(utils::read.delim(localpath, header = FALSE, 
+                                   stringsAsFactors = FALSE))[[1]][1]
   
   packageStartupMessage(paste("DataflowR Data Directory:",
                               fdir, "\n", "To change, modify:", localpath))
                         
   options("fdir" = fdir)
-  
 }
-
-
-
-
